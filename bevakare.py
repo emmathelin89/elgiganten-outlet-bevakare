5d7fd98752269efad7d805bb3040cfd40f9e69ac
emmathelin89 <emmathelin89@gmail.com>
2026-08-18 23:55:28 +0200

    Update bevakare.py

--- bevakare.py
+++ bevakare.py
@@ -1,1 +1,19 @@
-print("Elgiganten-bevakaren fungerar!")
+import requests
+
+url = "https://www.elgiganten.se/outlet"
+
+response = requests.get(
+    url,
+    headers={
+        "User-Agent": "Mozilla/5.0"
+    },
+    timeout=20
+)
+
+print("Statuskod:", response.status_code)
+print("Antal tecken:", len(response.text))
+
+if response.status_code == 200:
+    print("Elgigantens sida kunde hämtas!")
+else:
+    print("Något gick fel.")
