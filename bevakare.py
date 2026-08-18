f49c3988867adc97ff98cd6bb5754afe99bb0a42
emmathelin89 <emmathelin89@gmail.com>
2026-08-19 00:14:23 +0200

    Update bevakare.py

--- bevakare.py
+++ bevakare.py
@@ -1,19 +1,30 @@
 import requests
 
-url = "https://www.elgiganten.se/outlet"
+urls = {
+    "Laptop Outlet": "https://www.elgiganten.se/outlet/outlet-datorer-kontor/outlet-laptop/page-1",
+    "Gaming Laptop Outlet": "https://www.elgiganten.se/outlet/outlet-gaming/outlet-gaming-laptop"
+}
 
-response = requests.get(
-    url,
-    headers={
-        "User-Agent": "Mozilla/5.0"
-    },
-    timeout=20
-)
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
 
-print("Statuskod:", response.status_code)
-print("Antal tecken:", len(response.text))
+for name, url in urls.items():
+    print()
+    print("=" * 50)
+    print(name)
+    print(url)
+    print("=" * 50)
 
-if response.status_code == 200:
-    print("Elgigantens sida kunde hämtas!")
-else:
-    print("Något gick fel.")
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            timeout=20
+        )
+
+        print("Statuskod:", response.status_code)
+        print("Antal tecken:", len(response.text))
+
+    except Exception as e:
+        print("Fel:", e)
